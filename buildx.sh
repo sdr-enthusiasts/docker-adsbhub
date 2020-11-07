945d867c7d91b508c7117b2528aa6300c7c49a0e
Mike Nye <mike.nye@gmail.com>
2020-11-07 15:39:12 +0800

    Remove nohealthcheck and add armv6

--- buildx.sh
+++ buildx.sh
@@ -5,7 +5,7 @@ set -xe
 
 REPO=mikenye
 IMAGE=adsbhub
-PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64"
+PLATFORMS="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
 
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
@@ -23,10 +23,10 @@ docker buildx build -t "${REPO}/${IMAGE}:${VERSION}" --compress --push --platfor
 
 # BUILD NOHEALTHCHECK VERSION
 # Modify dockerfile to remove healthcheck
-sed '/^HEALTHCHECK /d' < Dockerfile > Dockerfile.nohealthcheck
+# sed '/^HEALTHCHECK /d' < Dockerfile > Dockerfile.nohealthcheck
 
-# Build & push latest
-docker buildx build -f Dockerfile.nohealthcheck -t ${REPO}/${IMAGE}:latest_nohealthcheck --compress --push --platform "${PLATFORMS}" .
+# # Build & push latest
+# docker buildx build -f Dockerfile.nohealthcheck -t ${REPO}/${IMAGE}:latest_nohealthcheck --compress --push --platform "${PLATFORMS}" .
 
-# If there are version differences, build & push with a tag matching the build date
-docker buildx build -f Dockerfile.nohealthcheck -t "${REPO}/${IMAGE}:${VERSION}_nohealthcheck" --compress --push --platform "${PLATFORMS}" .
+# # If there are version differences, build & push with a tag matching the build date
+# docker buildx build -f Dockerfile.nohealthcheck -t "${REPO}/${IMAGE}:${VERSION}_nohealthcheck" --compress --push --platform "${PLATFORMS}" .
